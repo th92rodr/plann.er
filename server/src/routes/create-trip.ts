cacb0fd76aca23cd6817895e97a3d16802f5e2ff
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-13 16:37:53 -0300

    start fastify api and create create-trip route

---
+++ server/src/routes/create-trip.ts
@@ -0,0 +1,39 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import dayjs from 'dayjs'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/trips',
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
+        }),
+      },
+    },
+    async request => {
+      const { destination, starts_at: startsAt, ends_at: endsAt } = request.body
+
+      if (dayjs(startsAt).isBefore(new Date())) {
+        throw new Error('Invalid trip start date.')
+      }
+
+      if (dayjs(endsAt).isBefore(startsAt)) {
+        throw new Error('Invalid trip end date.')
+      }
+
+      const trip = await prisma.trip.create({
+        data: { destination, starts_at: startsAt, ends_at: endsAt },
+      })
+
+      return { trip_id: trip.id }
+    }
+  )
+}
