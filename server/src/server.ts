9003695cd8c548122a0bb24ee20931164e7c7033
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-13 16:37:53 -0300

    Server: start fastify api and create create-trip route

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify'
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+import { createTrip } from './routes/create-trip'
+
+const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createTrip)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running')
+})
