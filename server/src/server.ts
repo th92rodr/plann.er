6b5a618e3e3376c789fd8e546e37d06c6d7617a7
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-13 18:09:37 -0300

    Server: create confirm-trip route and add logic to send a confirmation email to all trip participants

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,21 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+
+import { confirmTrip } from './routes/confirm-trip'
 import { createTrip } from './routes/create-trip'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
+app.register(confirmTrip)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
