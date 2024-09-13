a0ed7aea4c79017ca533c7c9cc4f8749e06c3903
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-13 18:11:42 -0300

    Server: create confirm-participant route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import cors from '@fastify/cors'
 import fastify from 'fastify'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 
+import { confirmParticipant } from './routes/confirm-participant'
 import { confirmTrip } from './routes/confirm-trip'
 import { createTrip } from './routes/create-trip'
 
@@ -16,6 +17,7 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipant)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
