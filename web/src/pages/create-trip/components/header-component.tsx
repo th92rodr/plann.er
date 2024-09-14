127df4b1a881434cefb6e12150a87cf5efe015c7
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-14 18:17:56 -0300

    separate create-trip page in multiple components

---
+++ web/src/pages/create-trip/components/header-component.tsx
@@ -0,0 +1,8 @@
+export function HeaderComponent() {
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <img src="/logo.svg" alt="plann.er" />
+      <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+    </div>
+  )
+}
