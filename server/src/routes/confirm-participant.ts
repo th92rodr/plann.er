b385e5ee2e65fcb1327228bda0df499c70bf3fb8
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-14 01:16:30 -0300

    add and validate env variables

--- server/src/routes/confirm-participant.ts
+++ server/src/routes/confirm-participant.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
+import { env } from '../env'
 import { ClientError } from '../errors/client-error'
 import { prisma } from '../lib/prisma'
 
@@ -27,7 +28,7 @@ export async function confirmParticipant(app: FastifyInstance) {
       }
 
       if (participant.is_confirmed) {
-        return reply.redirect(`http://localhost:3000/trips/${participant.trip_id}`)
+        return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
       }
 
       await prisma.participant.update({
@@ -35,7 +36,7 @@ export async function confirmParticipant(app: FastifyInstance) {
         data: { is_confirmed: true },
       })
 
-      return reply.redirect(`http://localhost:3000/trips/${participant.trip_id}`)
+      return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
     }
   )
 }
