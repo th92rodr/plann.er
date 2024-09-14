7a14557d11f112590a3ca64085d63e2114eb396b
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-14 18:17:56 -0300

    Web: separate create-trip page in multiple components

---
+++ web/src/pages/create-trip/components/footer-component.tsx
@@ -0,0 +1,15 @@
+export function FooterComponent() {
+  return (
+    <p className="text-zinc-500 text-sm">
+      Ao planejar sua viagem pela plann.er você automaticamente concorda <br /> com nossos{' '}
+      <a className="text-zinc-300 underline" href="#">
+        termos de uso
+      </a>{' '}
+      e{' '}
+      <a className="text-zinc-300 underline" href="#">
+        políticas de privacidade
+      </a>
+      .
+    </p>
+  )
+}
