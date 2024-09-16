4e448e705c30d4646e605cafe2eefeea3b7c0fbe
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-16 19:08:44 -0300

    Web: fetch api data on trip-details page links component

--- web/src/pages/trip-details/components/links-component.tsx
+++ web/src/pages/trip-details/components/links-component.tsx
@@ -1,22 +1,47 @@
 import { Link2, Plus } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 import { Button } from '../../../components/button'
+import { api } from '../../../lib/axios'
+
+interface Link {
+  id: string
+  title: string
+  url: string
+}
 
 export function LinksComponent() {
+  const { tripId } = useParams()
+  const [links, setLinks] = useState<Link[]>([])
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/links`).then(response => setLinks(response.data.links))
+  }, [tripId])
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links importantes</h2>
 
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-            <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-              https://www.airbnb.com.br/rooms/104700011
-            </a>
-          </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
+        {links.map(link => {
+          return (
+            <div key={link.id} className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">{link.title}</span>
+                <a
+                  href={link.url}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                >
+                  {link.url}
+                </a>
+              </div>
+              <Link2 className="text-zinc-400 size-5 shrink-0" />
+            </div>
+          )
+        })}
       </div>
 
       <Button type="button" variant="secondary" size="full">
