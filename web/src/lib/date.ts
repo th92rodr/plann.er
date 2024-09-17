a28ce84e15b594d407681c6d92c4fb94224ca256
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-17 13:59:37 -0300

    accept dates and strings params in the format date-range functions

--- web/src/lib/date.ts
+++ web/src/lib/date.ts
@@ -1,17 +1,20 @@
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 
-export function formatDateRangeWithFullMonth(start: Date, end: Date): string {
+export function formatDateRangeWithFullMonth(start: Date | string, end: Date | string): string {
   const month = format(start, 'LLLL', { locale: ptBR })
   return formatDateRange(start, end, month)
 }
 
-export function formatDateRangeWithAbbreviatedMonth(start: Date, end: Date): string {
+export function formatDateRangeWithAbbreviatedMonth(
+  start: Date | string,
+  end: Date | string
+): string {
   const month = format(start, 'LLL', { locale: ptBR })
   return formatDateRange(start, end, month)
 }
 
-function formatDateRange(start: Date, end: Date, month: string): string {
+function formatDateRange(start: Date | string, end: Date | string, month: string): string {
   const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1)
 
   const s = format(start, 'd', { locale: ptBR })
