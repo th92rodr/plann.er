3bc070bf02d4129e70fdf0d6825156b26be85464
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-29 17:07:42 -0300

    remove .env file from version control

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,10 +1,14 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production']),
+  SHUTDOWN_TIMEOUT: z.coerce.number(),
   DATABASE_URL: z.string().url(),
+  HOST: z.string().default('localhost'),
+  PORT: z.coerce.number().default(3333),
+  CORS_ORIGIN: z.string(),
   API_BASE_URL: z.string().url(),
   WEB_BASE_URL: z.string().url(),
-  PORT: z.coerce.number().default(3333),
 })
 
 export const env = envSchema.parse(process.env)
